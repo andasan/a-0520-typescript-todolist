65a46558b75a0dcb033e4d0bee297ccaba1a3af8
Francois Polo <andasan@gmail.com>
2020-12-10 14:29:40 -0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import TodoList from './TodosList';
+
+const App: React.FC = () => {
+  const todos = [{ id: 'ts1', text: 'Finish this course'}, { id: 'ts2', text: 'Finish eating'}]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList items={todos} />
     </div>
   );
 }
